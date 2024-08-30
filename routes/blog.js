731a8aa743f5da66989e6b5ddb6c46a8885fd55d
Pushpendra Kumar Ahirwar <rajpushpendra19@gmail.com>
2024-08-30 23:38:48 +0530

    Added Features

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const path = require('path')
 const multer = require('multer')
-const Blog = require('../models/blog')
+const Blog = require('../models/blog');
+const Comment = require("../models/comment");
 const router = Router();
 
+// Image Uploads
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, path.resolve('./public/uploads'))
@@ -13,15 +15,16 @@ const storage = multer.diskStorage({
         cb(null, fileName)
     }
 })
-
 const upload = multer({ storage: storage })
 
+//Add-POST routes
 router.get('/add-new', (req, res) => {
     return res.render('addBlog', {
         user: req.user
     })
 })
 
+// post routes for adding blog
 router.post('/', upload.single('coverImage'), async(req, res) => {
     const { title, body } = req.body
     const blog = await Blog.create({
@@ -30,7 +33,30 @@ router.post('/', upload.single('coverImage'), async(req, res) => {
         createdBy: req.user._id,
         coverImage: `/uploads/${req.file.filename}`
     })
-    return res.redirect(`/blog${blog._id}`)
+    return res.redirect(`/blog/${blog._id}`)
 
 })
+
+//get routes for gettting full blog
+router.get('/:id', async(req, res) => {
+    const blog = await Blog.findById(req.params.id).populate('createdBy')
+    const comments = await Comment.find({ blogId: req.params.id }).populate('createdBy')
+    return res.render('blog', {
+        user: req.user,
+        blog,
+        comments
+    })
+
+})
+
+//comment post routes
+router.post('/comment/:blogId', async(req, res) => {
+    await Comment.create({
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id
+    })
+    res.redirect(`/blog/${req.params.blogId}`)
+})
+
 module.exports = router;
